47264955df73c8c33900456d5b4d1cd9419b033c
Iurii Betekhtin <31073623+you-ra@users.noreply.github.com>
2021-09-10 13:59:23 +0300

    color change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <motion.div
         style={{
-          backgroundColor: "orange",
+          backgroundColor: "green",
           width: "100px",
           borderRadius: "10px"
         }}
