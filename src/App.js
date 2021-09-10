9de249f9af3514eb6cf095b923d34db86c20b2cd
Iurii Betekhtin <31073623+you-ra@users.noreply.github.com>
2021-09-10 13:52:34 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { FramerTreeLayoutContext } from "framer-motion";
+import "./styles.css";
+import { motion } from "framer-motion";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <motion.div
+        style={{
+          backgroundColor: "orange",
+          width: "100px",
+          borderRadius: "10px"
+        }}
+        animate={{
+          scale: 2,
+          rotate: 45
+        }}
+      >
+        This game has no name
+      </motion.div>
+    </div>
+  );
+}
